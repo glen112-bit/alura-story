35cd550d33c81465f36be6f6dfc62a098f341de0
Glen112 <escobar.glen@gmail.com>
2024-03-21 02:11:13 -0300

    a2.3

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { styled } from 'styled-components';
 
+const StyledButton = styled.button`
+cursor: pointer;
+border: 0;
+padding: 8px 14px;
+transition: 0.3s;
+`
 interface ButtonProps {
   disabled?: boolean;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
@@ -7,11 +14,11 @@ interface ButtonProps {
 }
 export default function Button({ children, ...props }: ButtonProps) {
   return (
-    <button
+    <StyledButton
       {...props}
     >
       {children}
-    </button>
+    </StyledButton>
   );
 }
 
